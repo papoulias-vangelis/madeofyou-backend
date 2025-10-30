2019bc8b9c69c6648c45c3540d407c87a15327a2
papoulias-vangelis <papvan@gmail.com>
2025-10-30 23:10:48 +0200

    added appId

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -10,6 +10,7 @@ export default defineCliConfig({
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/cli#auto-updates
      */
+    appId: 'f62xmg1zvo04cu27fe4vxqtj',
     autoUpdates: true,
   }
 })
